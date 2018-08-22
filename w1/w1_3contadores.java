f313f9479497f34811a2a9d046d70d50feb568a3
ccr1992 <cristobalcerv@gmail.com>
2018-08-22 14:25:56 +0200

    condición de parada

--- w1/w1_3contadores.java
+++ w1/w1_3contadores.java
@@ -1,5 +1,5 @@
     import java.util.*;
-    
+
 
 	public class w1_3contadores{
 		public static void main(String[] args) {
@@ -16,19 +16,24 @@
 			Queue<int[]> cola=new LinkedList<int[]>();
 			cola.add(nodoInicial);
 			nodoContainsNovelty(nodoInicial, novelties);
-			
-			while (!cola.isEmpty()){
+
+      boolean encontrado = false;
+			while (!cola.isEmpty() && !encontrado){
 				cont=cola.poll();
+        if (nodoEsObjetivo(cont, nodoFinal)){
+          encontrado =  true;
+        }
 				for (int i=0 ; i< acciones.length; i++){
 					nuevoNodo = actuar(acciones[i], cont);
-					
+
 					if (nodoContainsNovelty(nuevoNodo, novelties))
 					{
+
 							//agregamos al arbol
 							cola.add(nuevoNodo);
 					}
-					
-						
+
+
 				}
 				System.out.println("Actual  :" + cont[0]+" "+ cont[1]+" "+ cont[2] );
 				//System.out.println(novelties );
@@ -38,13 +43,13 @@
 
 			array con los elementos desplegados (recorrido en anchura)
 
-			
+
 
 			modo libreria*/
 		}
 
 		public static int[] actuar(String act, int[]estado){
-		  
+
 			int e[]= estado.clone();
 			switch(act){
 			  case "incrementar1":
@@ -59,7 +64,7 @@
 			}
 			return e;
 		}
-	  
+
 		public static boolean nodoContainsNovelty(int[] estado, ArrayList <Tuple> novelties){
 			Tuple t;
 			boolean containsNovelty = false;
@@ -71,12 +76,18 @@
 					novelties.add(t);
 
 				}
-				
+
 			}
 			return containsNovelty;
-	
 		}
-	  
+
+    public static boolean nodoEsObjetivo(int[] estado, int[] estadoObjetivo){
+      for (int i = 0; i< estado.length ; i++ ){
+        if (estado[i] == estadoObjetivo[i]) return true;
+      }
+      return false;
+    }
+
 	}
 	class Tuple{
 		private int cont;
@@ -87,11 +98,11 @@
 		  this.val = val;
 
 		}
-		
+
 		public String toString() {
 			return cont + ": " + val;
 		}
-		
+
 		@Override
 		public boolean equals(Object obj) {
 			Tuple t = (Tuple)obj;
