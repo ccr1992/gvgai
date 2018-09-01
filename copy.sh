2c7e47993e69f6f943ff24d6c5908e919d064a07
ccr1992 <cristobalcerv@gmail.com>
2018-09-01 13:02:57 +0200

    copiamos el randomAgent

---
+++ copy.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+
+
+
+
+
+
+
+cp -r ../../gvgai-803dc6b1aa0044548c455875926dc25390f11e3b/src/controllers/singlePlayer/cristobal .
+
---
+++ cristobal/ExtraccionCaracteristicas.java
@@ -0,0 +1,12 @@
+package controllers.singlePlayer.cristobal;
+
+import core.game.StateObservation;
+
+public class ExtraccionCaracteristicas{
+  /**
+  *Obtenemos un array de celdas con el número
+  **/
+  public static void convertir(StateObservation so){
+    System.out.println("conviritiendo estado");
+  }
+}
